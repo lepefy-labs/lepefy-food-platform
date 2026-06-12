113d50cf0020da6c454649325a02a62fe2e91338
robertinsmartinvestor-gif <robertin.smartinvestor@gmail.com>
2026-06-12 10:48:39 +0200

    Update manifest.ts

--- apps/storefront/src/app/manifest.ts
+++ apps/storefront/src/app/manifest.ts
@@ -20,13 +20,13 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
         src:     '/icons/icon-192.png',
         sizes:   '192x192',
         type:    'image/png',
-        purpose: 'any maskable',
+        purpose: 'maskable',
       },
       {
         src:     '/icons/icon-512.png',
         sizes:   '512x512',
         type:    'image/png',
-        purpose: 'any maskable',
+        purpose: 'maskable',
       },
     ],
   };
